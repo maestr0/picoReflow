d921718c278188e179cd40e751b5f7dac9021e43
Pawel Raszewski <raszewski@gmail.com>
2016-12-10 18:05:40 -0500

    maybe a fix

---
+++ config.py
@@ -0,0 +1,79 @@
+import logging
+
+########################################################################
+#
+#   General options
+
+### Logging
+log_level = logging.INFO
+log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+#logging.basicConfig(filename='oven.log',level=logging.DEBUG)
+### Server
+listening_ip = "0.0.0.0"
+listening_port = 80
+
+### Cost Estimate
+kwh_rate        = 0.26  # Rate in currency_type to calculate cost to run job
+currency_type   = "EUR"   # Currency Symbol to show when calculating cost to run job
+
+########################################################################
+#
+#   GPIO Setup (BCM SoC Numbering Schema)
+#   https://www.raspberrypi.org/documentation/usage/gpio-plus-and-raspi2/images/gpio-numbers-pi2.png
+#
+#   Check the RasPi docs to see where these GPIOs are
+#   connected on the P1 header for your board type/rev.
+#   These were tested on a Pi B Rev2 but of course you
+#   can use whichever GPIO you prefer/have available.
+
+### Outputs
+gpio_heat = 15  # Switches zero-cross solid-state-relay
+gpio_cool = 14  # Regulates PWM for 12V DC Blower
+gpio_air  = 18   # Switches 0-phase det. solid-state-relay
+
+### Inputs
+gpio_door = 7
+
+### Thermocouple Adapter selection (MAX31855 or MAX6675)
+max31855 = 1
+max6675 = 0
+### select this and unselet the two above if you use hardware SPI 0 on Raspberry Pi 2 B
+max31855_hardware_spi = 0
+
+### Thermocouple SPI Connection
+gpio_sensor_cs = 17
+gpio_sensor_clock = 27
+gpio_sensor_data = 22
+
+
+########################################################################
+#
+#   PID parameters
+
+pid_ki = 0.1  # Integration
+pid_kd = 0.4  # Derivative
+pid_kp = 0.5  # Proportional
+
+
+########################################################################
+#
+#   Simulation parameters
+
+sim_t_env      = 25.0   # deg C
+sim_c_heat     = 100.0  # J/K  heat capacity of heat element
+sim_c_oven     = 2000.0 # J/K  heat capacity of oven
+sim_p_heat     = 3500.0 # W    heating power of oven
+sim_R_o_nocool = 1.0    # K/W  thermal resistance oven -> environment
+sim_R_o_cool   = 0.05   # K/W  " with cooling
+sim_R_ho_noair = 0.1    # K/W  thermal resistance heat element -> oven
+sim_R_ho_air   = 0.05   # K/W  " with internal air circulation
+
+
+########################################################################
+#
+#   Time and Temperature parameters
+
+temp_scale          = "c" # c = Celsius | f = Fahrenheit - Unit to display
+time_scale_slope    = "s" # s = Seconds | m = Minutes | h = Hours - Slope displayed in temp_scale per time_scale_slope
+time_scale_profile  = "s" # s = Seconds | m = Minutes | h = Hours - Enter and view target time in time_scale_profile
+
